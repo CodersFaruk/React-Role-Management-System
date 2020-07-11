df2914d5b662375c8fdef53f97aa3f9f678fb995
CodersFaruk <mdomorfaruk9911@gmail.com>
2020-07-11 11:23:06 +0600

    react full project setup complete.(boiler boilerplate template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './assets/css/global.css';
+import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React JS</h1>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
     </div>
   );
 }
